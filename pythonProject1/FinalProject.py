a0f65b960591a74544d4e23b2d453f15452d01e6
schmi954 <schmidtjonmlg@gmail.com>
2022-05-10 20:16:06 -0500

    FinalProject.3

--- pythonProject1/FinalProject.py
+++ pythonProject1/FinalProject.py
@@ -1,9 +1,26 @@
+import webbrowser
+
 #initial dialogue
 print("House music player")
 print("\n")
 print("Please select which option you would like")
 
+#Initial Dictionary
 logindic = {}
+
+class VideoClass:
+    file = "https://www.youtube.com/watch?v=3o8w042VdOk"
+
+
+
+
+
+
+
+
+
+
+
 
 #This is the function for creating the dictionary username and password association
 def making_dictionary(lgdic, username, password):
@@ -17,6 +34,7 @@ def create_psswd():
     y = usernme[0:2]
     t = usernme[3:]
 
+    #Here I mix up the letters a bit so that passwords are different from username
     psswdfst = t + y
     psswdfst = psswdfst.replace("e", "z")
     psswdfst = psswdfst.replace("o", "f")
@@ -34,23 +52,46 @@ def create_psswd():
     logindic.update(updated_dic)
 
 
+#Here is where I check to see if the username and password exist in the dictionary
+def login_structure():
+    username = input("What is your username:  ")
+    password = input("What is your password:  ")
+
+    if username in logindic and logindic[username] == password:
+        print("succesfully loged in")
+    else:
+        print("User does not exist")
+
+
+
+
+
+#Here I print the opening menu structure
 def menu_structure():
     print("MENU-------------")
     print("1. Login")
     print("2. Create")
     print("3. Exit")
 
+#Here I call the menu structure and the first value for the user to select an option
 menu_structure()
 choice = int(input("Please select an option:   "))
 
+#This is my decision structure that takes the users input from menu
 while choice != 3:
     if choice == 1:
-        print("Choice 1")
+        login_structure()
+        videofile = VideoClass()
+        webbrowser.open(videofile.file)
+
+
+
     elif choice == 2:
         create_psswd()
     else:
         print("Please select one of the options")
 
+#Here I allow for the user to do the program again if they want to
     menu_structure()
     choice = int(input("Please select an option:   "))
 
